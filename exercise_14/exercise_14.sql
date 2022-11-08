494760a4c3052c8719e711cad3f7d8962fc3f6da
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-11-08 17:28:18 -0600

    pass 15

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -3,8 +3,8 @@
 SELECT
     category.name, AVG(film.length)
 FROM
- category
- left join film_category on film_category.category_id = category.category_id
- left join film on film.film_id = film_category.film_id
+    category
+    left join film_category on film_category.category_id = category.category_id
+    left join film on film.film_id = film_category.film_id
 group by
     category.name
