2e0b1147bd700ab312d09654da456ce4447a2fe9
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-11-08 17:23:42 -0600

    pass test 14

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,10 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT
+    category.name, AVG(film.length)
+FROM
+ category
+ left join film_category on film_category.category_id = category.category_id
+ left join film on film.film_id = film_category.film_id
+group by
+    category.name
