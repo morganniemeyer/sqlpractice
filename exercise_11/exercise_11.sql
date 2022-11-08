e15e7cb104e578c5dff7372ce712264999357f46
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-11-08 16:45:52 -0600

    pass 11

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,14 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT
+    film.title, COUNT(rental.inventory_id)
+FROM
+    film
+    LEFT join inventory on film.film_id = inventory.film_id
+    LEFT JOIN rental on inventory.inventory_id = rental.inventory_id
+
+GROUP BY
+    film.film_id
+ORDER BY COUNT(rental.inventory_id) DESC
+LIMIT 10
